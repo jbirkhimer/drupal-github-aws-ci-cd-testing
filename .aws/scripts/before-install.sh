0e4759441402965fd68b2d369088371d636af202
jbirkhimer <jbirkhimer@quotient-inc.com>
2023-09-07 16:50:06 -0400

    - add mariadb for mysqldump to drupal cloudformation template - update codedeploy appspec scripts - set version in dev site name to 0.0.2

--- .aws/scripts/before-install.sh
+++ .aws/scripts/before-install.sh
@@ -3,7 +3,7 @@
 GIT_REPO="jbirkhimer/drupal-github-aws-ci-cd-testing"
 PROJECT_DIR="/var/www/drupal/$(basename "$GIT_REPO")"
 BACKUP_DIR="/home/ec2-user/backups"
-BACKUP_FILE="${BACKUP_DIR}/archive-${DEPLOYMENT_ID}_$(date +"%Y-%m-%d").tar.gz"
+BACKUP_FILE="${BACKUP_DIR}/archive_$(date +"%Y-%m-%d").tar.gz"
 # run drush commands as needed
 cd "$PROJECT_DIR"/web || { echo "ERROR: $PROJECT_DIR/web does not exist!"; exit 1; }
 
@@ -12,7 +12,7 @@ mkdir -p $BACKUP_DIR
 #tar -zcvf $BACKUP_DIR/archive-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").tar.gz#
 #../vendor/bin/drush sql:dump --gzip --result-file=$BACKUP_DIR/database-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").sql
 
-../vendor/bin/drush archive:dump -y --destination=BACKUP_FILE --exclude-code-paths=web/sites/default/settings.php
+../vendor/bin/drush archive:dump -y --destination=$BACKUP_FILE --exclude-code-paths=web/sites/default/settings.php
 
 ../vendor/bin/drush state:set system.maintenance_mode TRUE
 
