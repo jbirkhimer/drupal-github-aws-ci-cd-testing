a16ac8bc083157bb0823c5d2ddcb33ba7cfb47ab
jbirkhimer <jbirkhimer@quotient-inc.com>
2023-09-07 15:17:33 -0400

    update drupal cloudformation template and codedeploy appspec scripts

--- .aws/scripts/after-install.sh
+++ .aws/scripts/after-install.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-PROJECT_DIR="drupal-github-aws-ci-cd-testing"
+PROJECT_DIR="/var/www/drupal/drupal-github-aws-ci-cd-testing"
 
-cd /var/www/drupal/$PROJECT_DIR
+cd $PROJECT_DIR || { echo "ERROR: $PROJECT_DIR does not exist!"; exit 1; }
 
 # (optional) sometimes get timeout error 'Install of drupal/core failed The following exception is caused by a process timeout' during composer install
 /usr/local/bin/composer config process-timeout 2000
 
-/usr/local/bin/composer install
+/usr/local/bin/composer install --no-interaction
 
 # run drush commands as needed
-cd /var/www/drupal/$PROJECT_DIR/web
+cd $PROJECT_DIR/web || { echo "ERROR: $PROJECT_DIR/web does not exist!"; exit 1; }
 
 # fix for bug with hook_install during site:install only allows using existing-config for minimal install profile
 # see https://www.drupal.org/project/drupal/issues/2982052 and https://www.drupal.org/node/2897299
 sed -i 's|standard|minimal|g' ../config/sync/default/core.extension.yml
 
-../vendor/bin/drush site:install -y \
---account-name=admin \
---account-pass=admin \
---existing-config
+chmod 755 $PROJECT_DIR/vendor/drush
 
-../vendor/bin/drush deploy
+../vendor/bin/drush deploy -y
 
 ../vendor/bin/drush state:set system.maintenance_mode FALSE
 
 ../vendor/bin/drush cache:rebuild
 
 # permissions
-# https://www.drupal.org/forum/support/post-installation/2016-09-22/file-and-directory-permissions-lets-finally-get-this
-find /var/www/drupal/$PROJECT_DIR -type d -exec chmod 755 {} +
-find /var/www/drupal/$PROJECT_DIR -type f -exec chmod 644 {} +
-chmod 444 /var/www/drupal/$PROJECT_DIR/web/.htaccess
-chmod 555 /var/www/drupal/$PROJECT_DIR/web/sites/default
-chmod 400 /var/www/drupal/$PROJECT_DIR/web/sites/default/settings.php
-chmod 755 /var/www/drupal/$PROJECT_DIR/vendor/drush/drush/drush
-find /var/www/drupal/$PROJECT_DIR/web/sites/default/files -type d -exec chmod 755 {} +
-find /var/www/drupal/$PROJECT_DIR/web/sites/default/files -type f -exec chmod 664 {} +
+find $PROJECT_DIR -type d -exec chmod 755 {} +
+find $PROJECT_DIR -type f -exec chmod 644 {} +
+chmod 444 $PROJECT_DIR/web/.htaccess
+chmod 555 $PROJECT_DIR/web/sites/default
+chmod 400 $PROJECT_DIR/web/sites/default/settings.php
+chmod 755 $PROJECT_DIR/vendor/drush
+find $PROJECT_DIR/web/sites/default/files -type d -exec chmod 755 {} +
+find $PROJECT_DIR/web/sites/default/files -type f -exec chmod 664 {} +
 chown -R ec2-user:apache /var/www/drupal
--- .aws/scripts/before-install.sh
+++ .aws/scripts/before-install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 GIT_REPO="jbirkhimer/drupal-github-aws-ci-cd-testing"
-PROJECT_DIR=$(basename "$GIT_REPO")
-
+PROJECT_DIR="/var/www/drupal/$(basename "$GIT_REPO")"
+BACKUP_DIR="/home/ec2-user/backups"
+BACKUP_FILE="${BACKUP_DIR}/archive-${DEPLOYMENT_ID}_$(date +"%Y-%m-%d").tar.gz"
 # run drush commands as needed
-cd /var/www/drupal/$PROJECT_DIR/web
+cd "$PROJECT_DIR"/web || { echo "ERROR: $PROJECT_DIR/web does not exist!"; exit 1; }
 
-mkdir -p /home/ec2-user/backups
+mkdir -p $BACKUP_DIR
 
-#tar -zcvf /home/ec2-user/backups/archive-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").tar.gz#
-#../vendor/bin/drush sql:dump --gzip --result-file=/home/ec2-user/backups/database-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").sql
+#tar -zcvf $BACKUP_DIR/archive-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").tar.gz#
+#../vendor/bin/drush sql:dump --gzip --result-file=$BACKUP_DIR/database-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").sql
 
-../vendor/bin/drush archive:dump -y --destination=/home/ec2-user/backups/archive-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").tar.gz
+../vendor/bin/drush archive:dump -y --destination=BACKUP_FILE
 
 ../vendor/bin/drush state:set system.maintenance_mode TRUE
 
 ../vendor/bin/drush cache:rebuild
-
-#rm -f /var/www/app/drupal/$PROJECT_DIR/*
