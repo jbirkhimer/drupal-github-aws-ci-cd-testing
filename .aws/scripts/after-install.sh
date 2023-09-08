d6d50f5926d22f7357c26d77e53f958914124266
jbirkhimer <jbirkhimer@quotient-inc.com>
2023-09-08 11:12:03 -0400

    update codedeploy cloudformation template and codedeploy appspec scripts set version in site name to test changes get deployed

--- .aws/scripts/after-install.sh
+++ .aws/scripts/after-install.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-printenv
-echo "================================="
-printenv | grep ENV
-echo "---------------------------------"
-if [ "$DEPLOYMENT_GROUP_NAME" == "drupal-github-aws-ci-cd-testing-dg-dev" ]
-then
-  echo "Environment = dev"
-else
-  echo "Environment != dev, DEPLOYMENT_GROUP_NAME = $DEPLOYMENT_GROUP_NAME"
-fi
+export ENVIRONMENT=$DEPLOYMENT_GROUP_NAME
 
-PROJECT_DIR="/var/www/drupal/drupal-github-aws-ci-cd-testing"
+PROJECT_DIR="/var/www/drupal/$APPLICATION_NAME"
 
 cd $PROJECT_DIR || { echo "ERROR: $PROJECT_DIR does not exist!"; exit 1; }
 
+echo "Running composer install"
 # (optional) sometimes get timeout error 'Install of drupal/core failed The following exception is caused by a process timeout' during composer install
 /usr/local/bin/composer config process-timeout 2000
 
@@ -25,12 +17,16 @@ cd $PROJECT_DIR/web || { echo "ERROR: $PROJECT_DIR/web does not exist!"; exit 1;
 
 chmod u+x $PROJECT_DIR/vendor/bin/drush
 
+echo "Runing drush deploy"
 ../vendor/bin/drush deploy -y
 
+echo "Setting maintenance mode FALSE"
 ../vendor/bin/drush state:set system.maintenance_mode FALSE
 
+echo "Running cache:rebuild"
 ../vendor/bin/drush cache:rebuild
 
+echo "Setting permissions"
 # Change the group ownership of /var/www and its contents to the apache group
 chown -R ec2-user:apache /var/www
 chmod 2775 /var/www && find /var/www -type d -exec chmod 2775 {} \;
--- .aws/scripts/before-install.sh
+++ .aws/scripts/before-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GIT_REPO="jbirkhimer/drupal-github-aws-ci-cd-testing"
-PROJECT_DIR="/var/www/drupal/$(basename "$GIT_REPO")"
+PROJECT_DIR="/var/www/drupal/$APPLICATION_NAME"
 BACKUP_DIR="/home/ec2-user/backups"
-BACKUP_FILE="${BACKUP_DIR}/archive_$(date +"%Y-%m-%d").tar.gz"
+BACKUP_FILE="${BACKUP_DIR}/archive_DEPLOYMENT_ID-${DEPLOYMENT_ID}_$(date +"%Y-%m-%d").tar.gz"
+
 # run drush commands as needed
 cd "$PROJECT_DIR"/web || { echo "ERROR: $PROJECT_DIR/web does not exist!"; exit 1; }
 
@@ -12,8 +12,12 @@ mkdir -p $BACKUP_DIR
 #tar -zcvf $BACKUP_DIR/archive-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").tar.gz#
 #../vendor/bin/drush sql:dump --gzip --result-file=$BACKUP_DIR/database-$DEPLOYMENT_ID_$(date +"%Y-%m-%d").sql
 
+echo "Running drush archive:dump"
+echo "Savin backup to: $BACKUP_FILE"
 ../vendor/bin/drush archive:dump -y --destination=$BACKUP_FILE --exclude-code-paths=web/sites/default/settings.php
 
+echo "Setting maintenance mode TRUE"
 ../vendor/bin/drush state:set system.maintenance_mode TRUE
 
+echo "Running cache:rebuild"
 ../vendor/bin/drush cache:rebuild
